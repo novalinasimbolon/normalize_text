b48f055e15aa025bc52d368e441cc56d9f954041
Novalina Simbolon <novalina351@gmail.com>
2021-04-02 22:08:22 +0700

    preprocessing text

--- spam_sms/preproses.py
+++ spam_sms/preproses.py
@@ -7,9 +7,10 @@ def bacafile(sms):
     regex = r'(?:\B\+ ?62|\b0)(?: *[(-]? *\d(?:[ \d]*\d)?)? *(?:[)-] *)?\d+ *(?:[/)-] *)?\d+ *(?:[/)-] *)?\d+(?: *- *\d+)?'
     phone_remove = url_remove.str.replace(regex, '')
     # https://stackoverflow.com/questions/52093555/python-regular-expression-for-phone-numbers
-    
-    punctuation_remove = phone_remove.str.replace('[^\w\s?@]'.format(string.punctuation), ' ')
-    punctuation_remove2 = punctuation_remove.str.replace('?', ' ')
 
-    lower_case = punctuation_remove2.str.lower()
+    punc_no = '[^\w\s?@]'
+    punctuation_remove = phone_remove.str.replace(punc_no.format(string.punctuation), ' ')
+    punctuation_remove2 = punctuation_remove.str.replace('?', ' ')
+    
+    lower_case = punctuation_remove2.str.lower()    
     return lower_case
